0b5c1e6067e23f5587943ff171b746904d11cc32
Dan Levine <daniel.v.levine@gmail.com>
2025-05-17 17:43:47 -0400

    added an install script (just copies files)

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cp -r include/* /usr/local/include
+cp -r lib/* /usr/local/lib
+
+echo "gtsam installed"
+
+#cp -r usr/local/include/* /usr/local/include
+#cp -r usr/local/lib/* /usr/local/lib
+
+#echo "paho.mqtt.cpp installed"
